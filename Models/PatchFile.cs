0ec6d52c37bd37ec62fd8341cf4bc28cdfc2cc35
admin <admin@ngshp.com>
2026-09-25 00:47:56 +0700

    Add PatchFile model with properties

---
+++ Models/PatchFile.cs
@@ -0,0 +1,20 @@
+namespace NGPB.Launcher.Models;
+
+
+public class PatchFile
+{
+
+
+public string Name {get;set;}="";
+
+
+public string Url {get;set;}="";
+
+
+public long Size {get;set;}
+
+
+public string SHA256 {get;set;}="";
+
+
+}
