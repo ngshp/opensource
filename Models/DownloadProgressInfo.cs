54311277bdff7ae73b499d5c6c9867029ff2c5bf
admin <admin@ngshp.com>
2026-09-25 00:48:36 +0700

    Add DownloadProgressInfo model for download tracking

---
+++ Models/DownloadProgressInfo.cs
@@ -0,0 +1,23 @@
+namespace NGPB.Launcher.Models;
+
+
+public class DownloadProgressInfo
+{
+
+
+public string FileName {get;set;}="";
+
+
+public double Percentage {get;set;}
+
+
+public double Speed {get;set;}
+
+
+public long CurrentBytes {get;set;}
+
+
+public long TotalBytes {get;set;}
+
+
+}
