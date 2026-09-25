964af69c538945673347db0459398938e2c156b4
admin <admin@ngshp.com>
2026-09-25 22:28:56 +0700

    Refactor AppConfig properties and remove unused fields

--- Models/AppConfig.cs
+++ Models/AppConfig.cs
@@ -4,28 +4,10 @@ namespace NGPB.Launcher.Models;
 public class AppConfig
 {
 
-    public string LauncherVersion { get; set; } = "1.0.0";
+    public string ApiUrl {get;set;} = "";
 
+    public string Version {get;set;} = "";
 
-    public string GameName { get; set; } = "NGPB";
-
-
-    public string GameExe { get; set; } = "NGPB.exe";
-
-
-    public string ApiUrl { get; set; } = "";
-
-
-    public string PatchUrl { get; set; } = "";
-
-
-    public string Manifest { get; set; } = "manifest.json";
-
-
-    public bool AutoUpdate { get; set; } = true;
-
-
-    public bool RequireSHA256 { get; set; } = true;
-
+    public string GameExe {get;set;} = "";
 
 }
