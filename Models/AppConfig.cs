0a4b1511068326518f03b1e0df2b1f777dc7967c
admin <admin@ngshp.com>
2026-09-25 23:10:54 +0700

    Refactor AppConfig class to include new properties

--- Models/AppConfig.cs
+++ Models/AppConfig.cs
@@ -1,32 +1,20 @@
 namespace NGPB.Launcher.Models;
 
-
 public class AppConfig
 {
+    public string LauncherVersion { get; set; } = "1.0.0";
 
+    public string GameName { get; set; } = "NGPB";
+
+    public string GameExe { get; set; } = "NGPB.exe";
 
     public string ApiUrl { get; set; } = "";
 
-
-
-    public string LoginEndpoint { get; set; } = "";
-
-
-
     public string PatchUrl { get; set; } = "";
 
+    public string Manifest { get; set; } = "manifest.json";
 
+    public bool AutoUpdate { get; set; } = true;
 
-    public string Version { get; set; } = "";
-
-
-
-    public string ServerName { get; set; } = "";
-
-
-
-    public bool Maintenance { get; set; }
-
-
-
+    public bool RequireSHA256 { get; set; } = true;
 }
