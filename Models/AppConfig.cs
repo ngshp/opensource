b9905c4f1f046722b06f988d9017cf79936a7da8
admin <admin@ngshp.com>
2026-09-25 23:02:12 +0700

    Add ServerName and PatchUrl properties to AppConfig

--- Models/AppConfig.cs
+++ Models/AppConfig.cs
@@ -4,10 +4,14 @@ namespace NGPB.Launcher.Models;
 public class AppConfig
 {
 
+
     public string ApiUrl {get;set;} = "";
+
+    public string PatchUrl {get;set;} = "";
 
     public string Version {get;set;} = "";
 
-    public string GameExe {get;set;} = "";
+    public string ServerName {get;set;} = "";
+
 
 }
